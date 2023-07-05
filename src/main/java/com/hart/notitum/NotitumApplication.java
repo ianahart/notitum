be7eb2d90e4c5d7838ab71d5a8e60c46d412e60a
Ian Hart <ianhart7790@gmail.com>
2023-07-05 19:30:40 -0400

    feat: initial commit

---
+++ src/main/java/com/hart/notitum/NotitumApplication.java
@@ -0,0 +1,13 @@
+package com.hart.notitum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotitumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotitumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hart/notitum/NotitumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.notitum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotitumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
