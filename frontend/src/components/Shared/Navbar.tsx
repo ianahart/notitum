d65066ba20df2c9dd5d7a4e8482d696d35475480
Ian Hart <ianhart7790@gmail.com>
2023-07-14 20:13:29 -0400

    feat: give logo a link

--- frontend/src/components/Shared/Navbar.tsx
+++ frontend/src/components/Shared/Navbar.tsx
@@ -1,10 +1,17 @@
 import { Box, Flex, Image } from '@chakra-ui/react';
 import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 import AccountMenu from '../Navbar/AccountMenu';
 import NavMenus from '../Navbar/NavMenus';
 import MobileNavMenus from '../Navbar/MobileNavMenus';
+import { useContext } from 'react';
+import { UserContext } from '../../context/user';
+import { IUserContext } from '../../interfaces';
+import { slugify } from '../../util';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext) as IUserContext;
   const navMenuStyles = {
     justifyContent: 'space-evenly',
   };
@@ -15,11 +22,28 @@ const Navbar = () => {
     display: 'flex',
   };
 
+  const handleGoHome = () => {
+    if (user.loggedIn) {
+      navigate(`/${slugify(user.firstName, user.lastName)}/dashboard`, {
+        state: { userId: user.id },
+      });
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
     <Box minH="50px" bg="cover.primary">
       <Flex justifyContent="space-between" alignItems="center">
         <Flex alignItems="center">
-          <Image width="120px" height="50px" src={logo} alt="notitum logo" />
+          <Image
+            cursor="pointer"
+            onClick={handleGoHome}
+            width="120px"
+            height="50px"
+            src={logo}
+            alt="notitum logo"
+          />
           <Box display={['none', 'none', 'block']}>
             <NavMenus styles={navMenuStyles} />
           </Box>
